a266974655518a8bca1c1c714d3df72e7387cc0e
ankitaAtGit <48355393+ankitaAtGit@users.noreply.github.com>
2025-08-01 11:07:35 +0530

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,7 @@ export const links: LinksFunction = () => [
 export default function App() {
   return (
     <html lang="en">
-      <head></head>
+      <head>
         <script>
             var uzeraq = []; function uzera(method, args) { (!!uzeraMethods && uzeraMethods[method]) ? uzeraMethods[method](args) : uzeraq.push({ method: method, args: args }); } (function (u, z, e, r, a) { if (u["a_id"]) { return; } u["a_id"] = a; u["dm"] = u.location.hostname; u[r] = u[r] || []; u[e] = {}; var s = z.createElement("script"); s.async = true; s.src = "https://assets.gainserv.in/wrapper.dev.js?v=" + Date.now(); var t = z.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, document, "uzeraMethods", "uzeraDL", "I84N7SQKE7")
         </script>
