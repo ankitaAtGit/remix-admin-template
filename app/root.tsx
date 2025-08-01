97c6061e155ad0c093f5d9b616235e16c0530e32
ankitaAtGit <48355393+ankitaAtGit@users.noreply.github.com>
2025-08-01 11:10:08 +0530

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -27,9 +27,30 @@ export default function App() {
   return (
     <html lang="en">
       <head>
-        <script>
-            var uzeraq = []; function uzera(method, args) { (!!uzeraMethods && uzeraMethods[method]) ? uzeraMethods[method](args) : uzeraq.push({ method: method, args: args }); } (function (u, z, e, r, a) { if (u["a_id"]) { return; } u["a_id"] = a; u["dm"] = u.location.hostname; u[r] = u[r] || []; u[e] = {}; var s = z.createElement("script"); s.async = true; s.src = "https://assets.gainserv.in/wrapper.dev.js?v=" + Date.now(); var t = z.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, document, "uzeraMethods", "uzeraDL", "I84N7SQKE7")
-        </script>
+        <script
+  dangerouslySetInnerHTML={{
+    __html: `
+      var uzeraq = [];
+      function uzera(method, args) {
+        (!!uzeraMethods && uzeraMethods[method])
+          ? uzeraMethods[method](args)
+          : uzeraq.push({ method: method, args: args });
+      }
+      (function (u, z, e, r, a) {
+        if (u["a_id"]) { return; }
+        u["a_id"] = a;
+        u["dm"] = u.location.hostname;
+        u[r] = u[r] || [];
+        u[e] = {};
+        var s = z.createElement("script");
+        s.async = true;
+        s.src = "https://assets.gainserv.in/wrapper.dev.js?v=" + Date.now();
+        var t = z.getElementsByTagName("script")[0];
+        t.parentNode.insertBefore(s, t);
+      })(window, document, "uzeraMethods", "uzeraDL", "I84N7SQKE7");
+    `,
+  }}
+/>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
